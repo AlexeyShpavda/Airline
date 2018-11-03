178449679a87b9c1fa5985bd16a429e1d347aed0
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-03 19:57:53 +0300

    Create Necessary Enums for Transport Aircraft Hierarchy

---
+++ Interfaces/TransportAircraftInterfaces/Airplanes/PassengerAirplanes/Enums/FlightRangeClassification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.TransportAircraftInterfaces.Airplanes.PassengerAirplanes.Enums
+{
+    public enum FlightRangeClassification
+    {
+        LongHaul,
+        MediumHaul,
+        ShortHaul,
+        Local
+    }
+}
---
+++ Interfaces/TransportAircraftInterfaces/Enums/TransportedType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.TransportAircraftInterfaces.Enums
+{
+    public enum TransportedType
+    {
+        Passenger,
+        Cargo
+    }
+}

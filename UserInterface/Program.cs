f62ea73fbdf2d0831603e3e7ca87aac7f7b5fb93
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-05 00:18:50 +0300

    Calculate Total Capacity of Passenger and Cargo Aircrafts

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -6,6 +6,8 @@ using AirlineHierarchy.TransportAircrafts.Airplanes.PassengerAirplanes.NarrowBod
 using AirlineHierarchy.TransportAircrafts.Airplanes.PassengerAirplanes.RagionalAirliners.Models;
 using AirlineHierarchy.TransportAircrafts.Airplanes.PassengerAirplanes.WideBodyAirliners.Models;
 using Interfaces.TransportAircraftInterfaces;
+using Interfaces.TransportAircraftInterfaces.Airplanes.CargoAirplanes;
+using Interfaces.TransportAircraftInterfaces.Airplanes.PassengerAirplanes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,15 @@ namespace UserInterface
             airline.BuyAircrafts<Boeing737>(1);
             airline.BuyAircrafts<ComacARJ21>(1);
             airline.BuyAircrafts<CirrusSR22>(3);
+
+            var passengerAirplanes = airline.ShowAircraftsOfCertainType<IPassengerAirplane>();
+            var cargoAirplanes = airline.ShowAircraftsOfCertainType<ICargoAirplane>();
+
+            // Capacity for Passenger Aircrafts is Number of Seats
+            var seatsNumber = airline.GetInformationAboutTotal(passengerAirplanes, x => x.SeatsNumber);
+
+            // Capacity for Cargo Aircrafts is Volume of Cargo Compartment
+            var cargoCompartmentVolume = airline.GetInformationAboutTotal(cargoAirplanes, x => x.CargoCompartmentVolume);
         }
     }
 }

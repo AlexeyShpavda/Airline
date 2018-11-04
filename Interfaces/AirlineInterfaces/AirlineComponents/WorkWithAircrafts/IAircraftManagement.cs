f5d1c52f12e087917b7f0a72b6fda2069554f056
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-04 23:34:31 +0300

    Write Interfaces for Airline Components

---
+++ Interfaces/AirlineInterfaces/AirlineComponents/WorkWithAircrafts/IAircraftManagement.cs
@@ -0,0 +1,25 @@
+﻿using Interfaces.TransportAircraftInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.AirlineInterfaces.AirlineComponents.WorkWithAircrafts
+{
+    public interface IAircraftManagement
+    {
+        void BuyAircrafts<T>(ICollection<ITransportAircraft> transportAircrafts, int quantity)
+            where T : ITransportAircraft, new();
+
+        void SellAircraft(ICollection<ITransportAircraft> transportAircrafts, ITransportAircraft transportAircraft);
+
+        IEnumerable<ITransportAircraft> ShowAllAircraft(IEnumerable<ITransportAircraft> transportAircrafts);
+
+        IEnumerable<T> ShowAircraftsOfCertainType<T>(IEnumerable<ITransportAircraft> transportAircrafts)
+            where T : ITransportAircraft;
+
+        int GetInformationAboutTotal<T>(IEnumerable<T> transportAircrafts, Func<T, int> summationСriteria)
+            where T : ITransportAircraft;
+    }
+}
---
+++ Interfaces/AirlineInterfaces/AirlineComponents/WorkWithAircrafts/IAircraftSelection.cs
@@ -0,0 +1,21 @@
+﻿using Interfaces.TransportAircraftInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.AirlineInterfaces.AirlineComponents.WorkWithAircrafts
+{
+    public interface IAircraftSelection
+    {
+        IEnumerable<ITransportAircraft> SortAircraftAscending<T>(IEnumerable<ITransportAircraft> transportAircrafts,
+            Func<ITransportAircraft, T> sortingCriteria);
+
+        IEnumerable<ITransportAircraft> SortAircraftDescending<T>(IEnumerable<ITransportAircraft> transportAircrafts,
+            Func<ITransportAircraft, T> sortingCriteria);
+
+        IEnumerable<ITransportAircraft> SearchForSuitableAircraftByRange(IEnumerable<ITransportAircraft> transportAircrafts,
+            Func<ITransportAircraft, double> searchCriteria, double lowerBound, double upperBound);
+    }
+}

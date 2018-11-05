42a344d93eaa82327e90b2fc4d1ba93688ca61f8
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-05 12:55:22 +0300

    Remove Unused References

--- AirlineHierarchy/TransportAircrafts/Airplanes/PassengerAirplanes/NarrowBodyAirliners/NarrowBodyAirliner.cs
+++ AirlineHierarchy/TransportAircrafts/Airplanes/PassengerAirplanes/NarrowBodyAirliners/NarrowBodyAirliner.cs
@@ -1,7 +1,6 @@
 ﻿using Interfaces.TransportAircraftInterfaces.Airplanes.PassengerAirplanes.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
--- AirlineHierarchy/TransportAircrafts/Airplanes/PassengerAirplanes/PassengerAirplane.cs
+++ AirlineHierarchy/TransportAircrafts/Airplanes/PassengerAirplanes/PassengerAirplane.cs
@@ -3,7 +3,6 @@ using Interfaces.TransportAircraftInterfaces.Airplanes.PassengerAirplanes.Enums;
 using Interfaces.TransportAircraftInterfaces.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 

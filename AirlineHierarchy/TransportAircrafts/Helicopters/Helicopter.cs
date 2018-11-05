a3bc84b61f8ec18382f91ddb228d3d6433fd461f
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-06 00:47:04 +0300

    Create New Type of Aircraft - Helicopter

---
+++ AirlineHierarchy/TransportAircrafts/Helicopters/Helicopter.cs
@@ -0,0 +1,9 @@
+﻿using Interfaces.TransportAircraftInterfaces.Enums;
+
+namespace AirlineHierarchy.TransportAircrafts.Helicopters
+{
+    public abstract class Helicopter : TransportAircraft
+    {
+        public sealed override AircraftType AircraftType => AircraftType.Helicopter;
+    }
+}
